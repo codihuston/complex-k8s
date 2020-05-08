fa1ecb653bd9683e49b4c26f0db38336e39922c1
Codi Huston (Home) <codi.huston540@gmail.com>
2020-05-08 16:36:18 -0500

    add: deploy script
    - include github $SHA tag for our docker images

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# build images
+docker build -t codihuston/multi-docker-client:latest -t codihuston/multi-docker-client:$SHA -f ./client/Dockerfile ./client
+docker build -t codihuston/multi-docker-server:latest -t codihuston/multi-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t codihuston/multi-docker-worker:latest -t codihuston/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# push to docker hub (don't need to auth b/c we've done it already)
+docker push codihuston/multi-docker-client:latest
+docker push codihuston/multi-docker-server:latest
+docker push codihuston/multi-docker-worker:latest
+docker push codihuston/multi-docker-client:$SHA
+docker push codihuston/multi-docker-server:$SHA
+docker push codihuston/multi-docker-worker:$SHA
+
+# apply the k8s configuration files
+kubectl apply -f k8s
+
+# update the images
+kubectl set image deployments/server-deployment server=codihuston/multi-docker-server:$SHA
+kubectl set image deployments/client-deployment server=codihuston/multi-docker-client:$SHA
+kubectl set image deployments/worker-deployment server=codihuston/multi-docker-worker:$SHA
